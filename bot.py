55300c9c39ae5343936c02da4502fcbe9a5b1575
dbashkirov <DPaBashkirov@sberbank.ru>
2023-03-29 15:56:59 +0300

    edited bot

--- bot.py
+++ bot.py
@@ -10,6 +10,10 @@ bot = TeleBot(TOKEN)
 user_dict = {}
 
 
+def health():
+    return requests.get('http://localhost:8080/health')
+
+
 @bot.message_handler(commands=["start"])
 def start_message(message):
     bot.send_message(message.chat.id, WELCOME_MSG)
@@ -22,17 +26,15 @@ def about_message(message):
 
 @bot.message_handler(commands=["predict"])
 def predict(message):
+    user_dict[message.chat.id] = User(message.chat.id)
     msg = bot.send_message(message.chat.id, "Для акций какой компании ты хочешь получить прогноз?",
                            reply_markup=company_markup)
-    user_dict[message.chat.id] = User(message.chat.id)
     bot.register_next_step_handler(msg, ask_company)
 
 
 def ask_company(message):
     text = message.text
     if text.lower() not in ['газпром', 'яндекс', 'аэрофлот', 'сбербанк']:
-        # if text.lower() == 'назад':
-        #     bot.register_next_step_handler(message, predict)
         msg = bot.send_message(message.chat.id, "Неправильное название компании. Попробуй еще раз",
                                reply_markup=company_markup)
         bot.register_next_step_handler(msg, ask_company)
@@ -47,24 +49,27 @@ def ask_company(message):
 
 def ask_date(message):
     text = message.text
-    # if text.lower() == 'назад':
-    #     bot.register_next_step_handler(message, ask_company)
-    date = dateparser.parse(text)
-    if date is None:
-        msg = bot.send_message(message.chat.id, "Неправильный формат даты. Попробуй еще раз")
+    if text.lower() == 'назад':
+        msg = bot.send_message(message.chat.id, "Для акций какой компании ты хочешь получить прогноз?",
+                               reply_markup=company_markup)
+        bot.register_next_step_handler(msg, ask_company)
+    else:
+        date = dateparser.parse(text)
+        if date is None:
+            msg = bot.send_message(message.chat.id, "Неправильный формат даты. Попробуй еще раз")
+            bot.register_next_step_handler(msg, ask_date)
+        req_date = f"{date.day}/{date.month}/{date.year}"
+        company = user_dict[message.chat.id].company
+        data = {
+            "rq_id": "1",
+            "company": company,
+            "req_date": req_date,
+        }
+        res = requests.post('http://localhost:8080/predict', data=json.loads(json.dumps(data)))
+        res_flg = res.json()['flg']
+        msg = bot.send_message(message.chat.id, f"Акции {REVERSE_MAP_COMPANIES[company]} {req_date} {RESULT_FLG[res_flg]}\nВведи новую дату или команду /predict, чтобы выбрать другую компанию",
+                               parse_mode="Markdown", reply_markup=default_markup)
         bot.register_next_step_handler(msg, ask_date)
-    req_date = f"{date.day}/{date.month}/{date.year}"
-    company = user_dict[message.chat.id].company
-    data = {
-        "rq_id": "1",
-        "company": company,
-        "req_date": req_date,
-    }
-    res = requests.post('http://localhost:8080/predict', data=json.loads(json.dumps(data)))
-    res_flg = res.json()['flg']
-    msg = bot.send_message(message.chat.id, f"Акции {REVERSE_MAP_COMPANIES[company]} {req_date} {RESULT_FLG[res_flg]}\nВведи новую дату или команду /predict, чтобы выбрать другую компанию",
-                           parse_mode="Markdown", reply_markup=default_markup)
-    bot.register_next_step_handler(msg, ask_date)
 
 
 @bot.message_handler(content_types=["text"])
